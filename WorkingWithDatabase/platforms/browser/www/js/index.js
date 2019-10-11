5972b7a12e2e620759829449015c05740d426409
Binh Do Quoc <binhdq@fpt.edu.vn>
2019-10-11 10:09:59 +0700

    aa

--- WorkingWithDatabase/platforms/browser/www/js/index.js
+++ WorkingWithDatabase/platforms/browser/www/js/index.js
@@ -42,13 +42,21 @@ function viewData(){
   }, null);
 }); 
 }
+ $("#create").click(function(event) {
+      createDatabase();
+  });
 function createDatabase(){
     var db = openDatabase('mydb', '1.0', 'Test DB', 2 * 1024 * 1024);  
 
     db.transaction(function (tx) { 
-     tx.executeSql('CREATE TABLE IF NOT EXISTS LOGS (id unique, log)');
-     tx.executeSql('INSERT INTO LOGS (id, log) VALUES (1, "foobar")'); 
-     tx.executeSql('INSERT INTO LOGS (id, log) VALUES (2, "logmsg")'); 
-     console.log('done! ');
+    var id = document.getElementById("id").value;
+    var log = document.getElementById("log").value;  
+    var sql = 'INSERT INTO LOGS (id, log) VALUES (' + 
+    id + 
+    ', "' + 
+    log +' ")'
+    tx.executeSql('CREATE TABLE IF NOT EXISTS LOGS (id unique, log)');
+    tx.executeSql(sql); 
+    alert('Insertion done! ');
  });
 }
