f5d7691f440684d8dac7559034651766f332e1b9
Binh Do Quoc <binhdq@fpt.edu.vn>
2019-10-11 21:01:34 +0700

    working example for view and search

--- new database/loadData.js
+++ new database/loadData.js
@@ -1,23 +1,23 @@
-// var db = openDatabase('mydb', '1.0', 'my first database', 2 * 1024 * 1024);
-// db.transaction(function (tx) {
-//   	tx.executeSql('SELECT * FROM product', [], function (tx, results) {
-// 	var len = results.rows.length, i;
-//   	var table = document.getElementById("mytable");
-//   	for (i = 0; i < len; i++) {
+var db = openDatabase('mydb', '1.0', 'my first database', 2 * 1024 * 1024);
+db.transaction(function (tx) {
+  	tx.executeSql('SELECT * FROM product', [], function (tx, results) {
+	var len = results.rows.length, i;
+  	var table = document.getElementById("mytable");
+  	for (i = 0; i < len; i++) {
 
-// 		//insert new row at the bottom
-// 		var row = table.insertRow(-1);
-// 		var idCell = row.insertCell(0);
-// 		var nameCell = row.insertCell(1);
-// 		var priceCell = row.insertCell(2);
+		//insert new row at the bottom
+		var row = table.insertRow(-1);
+		var idCell = row.insertCell(0);
+		var nameCell = row.insertCell(1);
+		var priceCell = row.insertCell(2);
   		
-//   		idCell.innerHTML = results.rows.item(i).id;
-//     	nameCell.innerHTML =results.rows.item(i).name;
-//     	priceCell.innerHTML = results.rows.item(i).price;
+  		idCell.innerHTML = results.rows.item(i).id;
+    	nameCell.innerHTML =results.rows.item(i).name;
+    	priceCell.innerHTML = results.rows.item(i).price;
 
-//   	}
-// });
-// });
+  	}
+});
+});
 
 $("#btnSearch").click(function(event) {
 	var db = openDatabase('mydb', '1.0', 'my first database', 2 * 1024 * 1024);
@@ -26,8 +26,12 @@ $("#btnSearch").click(function(event) {
   		, ['%' + $("#name").val() +'%'], function (tx, results) {
 	var len = results.rows.length, i;
   	var table = document.getElementById("mytable");
-  	alert('remove');
-  	$("tbody").children().remove();
+
+  	$(function()
+	  {
+	  $('table tr').not(':nth-child(1)').remove()
+	});
+
   	for (i = 0; i < len; i++) {
 		//insert new row at the bottom
 		var row = table.insertRow(-1);
