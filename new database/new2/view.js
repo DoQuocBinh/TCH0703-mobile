c8f4cdec5cf1a27edd1195ab89a790cacbd2c2f7
Binh Do Quoc <binhdq@fpt.edu.vn>
2019-10-18 12:06:09 +0700

    aa

--- new database/new2/view.js
+++ new database/new2/view.js
@@ -1,4 +1,9 @@
 function populateTable(results){
+	//delete all rows from table except the header
+	$(function()
+	{
+	  $('#mytable tr').not(':nth-child(1)').remove()
+	});
 	var len = results.rows.length, i;
 	var table = document.getElementById("mytable");
 	for (i = 0; i < len; i++) {
@@ -7,21 +12,33 @@ function populateTable(results){
 		var idCell = row.insertCell(0);
 		var nameCell = row.insertCell(1);
 		var priceCell = row.insertCell(2);
+		var removeCell = row.insertCell(3);
 
 		idCell.innerHTML = results.rows.item(i).id;
 		nameCell.innerHTML =results.rows.item(i).name;
 		priceCell.innerHTML = results.rows.item(i).price;
+		removeCell.innerHTML = "<button onclick='removeRow(this)' productid='"
+			+results.rows.item(i).id  + "'>Delete</button>"; 
 	}
 }
+function deleteRowFromDB(productid){
+	var db = openDatabase('mydb', '1.0', 'my first database', 2 * 1024 * 1024);
+		db.transaction(function (tx) {
+		  	var sql = "delete from product where id = ?";
+		  	tx.executeSql(sql,[productid]);
+		});
+}
+function removeRow(id){
+	var productid = id.getAttribute('productid');
+	alert('you are deleting ' + productid);
+	deleteRowFromDB(productid);
+	loadData();
+} 
+
 $(document).ready(function() {
 	loadData();
 	$("#searchBox").on("keyup",function(){
 		var nameSearch = $(this).val();
-		//delete all rows from table except the header
-		$(function()
-		{
-		  $('#mytable tr').not(':nth-child(1)').remove()
-		});
 		var db = openDatabase('mydb', '1.0', 'my first database', 2 * 1024 * 1024);
 		db.transaction(function(tx){
 			tx.executeSql('SELECT * FROM product where name like ?', 
@@ -31,7 +48,7 @@ $(document).ready(function() {
 	  			
 					});
 		});
-		});
+	});
 });
 
 function loadData(){
