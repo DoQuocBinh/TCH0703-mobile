3dad0da778b0ef46b940ba9aa2f83454226a1be0
Binh Do Quoc <binhdq@fpt.edu.vn>
2019-10-18 19:25:06 +0700

    aaa

--- new database/loadData.js
+++ new database/loadData.js
@@ -1,15 +1,21 @@
 $(document).ready(function() {
+  loadAll();
+  //register the event search
   $("#searchBox").keyup(function(event) {
-    var nameSearch = $("#searchBox").val();
-    var db = openDatabase('mydb', '1.0', 'my first database', 2 * 1024 * 1024);
-    db.transaction(function (tx) {
-      tx.executeSql('SELECT * FROM product where name like ?'
-        , ['%' + nameSearch +'%'], function (tx, results) {
-         loadDataToTable(results);
-       });
-    });
+      performSearch(); //call the search function
   });
 });
+
+function performSearch(){
+  var nameSearch = $("#searchBox").val();
+  var db = openDatabase('mydb', '1.0', 'my first database', 2 * 1024 * 1024);
+  db.transaction(function (tx) {
+    tx.executeSql('SELECT * FROM product where name like ?'
+      , ['%' + nameSearch +'%'], function (tx, results) {
+       loadDataToTable(results);
+     });
+  });
+}
 
 function loadDataToTable(results){
   var len = results.rows.length, i;
@@ -26,15 +32,35 @@ function loadDataToTable(results){
     var idCell = row.insertCell(0);
     var nameCell = row.insertCell(1);
     var priceCell = row.insertCell(2);
+    var removeCell = row.insertCell(3);
       
       idCell.innerHTML = results.rows.item(i).id;
       nameCell.innerHTML =results.rows.item(i).name;
       priceCell.innerHTML = results.rows.item(i).price
+      removeCell.innerHTML = "<button onclick='removeRow(this)' productId='" +  
+                            results.rows.item(i).id + "'>Delete</button>";
     }
 }
-var db = openDatabase('mydb', '1.0', 'my first database', 2 * 1024 * 1024);
-db.transaction(function (tx) {
-  	tx.executeSql('SELECT * FROM product', [], function (tx, results) {
-	 loadDataToTable(results);
-});
-});
+function removeRow(butt){
+  var id = butt.getAttribute("productId");
+  alert("Going to remove: " + id);
+  deleteFromDB(id);
+  loadAll();
+
+}
+function deleteFromDB(id){
+  var db = openDatabase('mydb', '1.0', 'my first database', 2 * 1024 * 1024);
+  db.transaction(function (tx) {
+    var sql = "delete from product where id = ?";
+    tx.executeSql(sql,[id]);
+  });
+}
+function loadAll(){
+  var db = openDatabase('mydb', '1.0', 'my first database', 2 * 1024 * 1024);
+  db.transaction(function (tx) {
+      tx.executeSql('SELECT * FROM product', [], function (tx, results) {
+     loadDataToTable(results);
+    });
+  });
+}
+
